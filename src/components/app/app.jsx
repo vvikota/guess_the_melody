fadc42025d7c2a6c509a0cd06d64635651093a57
vvikota <rymsha.viktor@gmail.com>
2020-04-03 12:37:13 +0300

    finish first lesson

---
+++ src/components/app/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import WelcomeScreen from "../welcom-screen/welcom-screen.jsx";
+
+const App = () => {
+  // const {errorCount, gameTime} = props;
+
+  return <WelcomeScreen
+    // time={gameTime}
+    // errorCount={errorCount}
+  />;
+
+  // return (<div>
+  //   <p>hello world</p>
+  //   </div>
+  //   );
+};
+
+// App.propTypes = {
+//   gameTime: PropTypes.number,
+//   errorCount: PropTypes.number
+// };
+
+export default App;
