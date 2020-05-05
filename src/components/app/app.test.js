3e8e3a111cb2f0fc5b3c7de77a76d703da8ef7ba
vvikota <rymsha.viktor@gmail.com>
2020-05-05 14:56:54 +0300

    add some test for App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {App} from "./app.jsx";
 
-it(`App correctly renders`, () => {
+it(`App correctly renders first screen`, () => {
   const tree = renderer
     .create(<App
       gameTime={10}
@@ -15,3 +15,33 @@ it(`App correctly renders`, () => {
 
   expect(tree).toMatchSnapshot();
 });
+
+it(`App correctly renders genre question screen`, () => {
+  const tree = renderer
+    .create(<App
+      gameTime={10}
+      questions={[]}
+      mistakes={0}
+      step={1}
+      renderScreen={jest.fn()}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it(`App correctly renders artist question screen`, () => {
+  const tree = renderer
+    .create(<App
+      gameTime={10}
+      questions={[]}
+      mistakes={0}
+      step={2}
+      renderScreen={jest.fn()}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+
