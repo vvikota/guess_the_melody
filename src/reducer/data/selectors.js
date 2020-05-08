43a2eb2da7c1a69acc804bc755902d4beec57f4c
vvikota <rymsha.viktor@gmail.com>
2020-05-08 12:20:30 +0300

    add reselect

--- src/reducer/data/selectors.js
+++ src/reducer/data/selectors.js
@@ -1,3 +1,4 @@
+import {createSelector} from "reselect";
 import NameSpace from "../name-spaces";
 
 const NAME_SPACE = NameSpace.DATA;
@@ -5,4 +6,22 @@ const NAME_SPACE = NameSpace.DATA;
 export const getQuestions = (state) => {
   return state[NAME_SPACE].questions;
 };
+
+export const getGenreQuestions = createSelector(
+    getQuestions,
+    (questions) => questions.filter((it) => it.type === `genre`)
+);
+
+const randomFilter = (_state) => {
+  return Math.random() > 0.5;
+};
+
+export const getRandomArtistQuestions = createSelector(
+    getQuestions,
+    randomFilter,
+
+    (resultOne, resultTwo) => {
+      return resultOne.filter((it) => resultTwo && it.type === `artist`);
+    }
+);
 
