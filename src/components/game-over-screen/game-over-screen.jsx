36af57d9d154fa8dbf0c9ce72facbbda534e87e0
vvikota <rymsha.viktor@gmail.com>
2020-05-14 22:53:38 +0300

    add resetGame to win Screen and GameOverScreen

--- src/components/game-over-screen/game-over-screen.jsx
+++ src/components/game-over-screen/game-over-screen.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const GameOverScreen = () => {
+const GameOverScreen = (props) => {
+
+  GameOverScreen.propTypes = {
+    onRelaunchButtonClick: PropTypes.func.isRequired,
+  };
+
   return <section className="result">
     <div className="result__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
     </div>
     <h2 className="result__title">Какая жалость!</h2>
     <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
-    <button className="replay" type="button">Попробовать ещё раз</button>
+    <button className="replay" type="button" onClick={props.onRelaunchButtonClick}>Попробовать ещё раз</button>
   </section>;
 };
 
--- src/components/win-screen/win-screen.jsx
+++ src/components/win-screen/win-screen.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const WinScreen = () => {
+const WinScreen = (props) => {
+  WinScreen.propTypes = {
+    onRelaunchButtonClick: PropTypes.func.isRequired,
+  };
+
   return <section className="result">
     <div className="result__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
@@ -8,7 +13,7 @@ const WinScreen = () => {
     <h2 className="result__title">Вы настоящий меломан!</h2>
     <p className="result__total">За 3 минуты и 25 секунд вы набрали 12 баллов (8 быстрых), совершив 3 ошибки</p>
     <p className="result__text">Вы заняли 2 место из 10. Это лучше чем у 80% игроков</p>
-    <button className="replay" type="button">Сыграть ещё раз</button>
+    <button className="replay" type="button" onClick={props.onRelaunchButtonClick}>Сыграть ещё раз</button>
   </section>;
 };
 
